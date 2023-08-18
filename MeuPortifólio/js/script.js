50ab06f656f36471285d16cecf25207449507f2d
PereiraLegend <pereirapro123@gmail.com>
2023-08-18 20:48:59 -0300

    Update script.js

--- MeuPortifólio/js/script.js
+++ MeuPortifólio/js/script.js
@@ -1,4 +1,5 @@
 // Parte Carrossel
+/*
 const imgs = document.getElementById("img")
 const img = document.querySelector("#img img")
 let idx = 0;
@@ -10,4 +11,24 @@ function carrossel(){
     
     imgs.style.transform = `translateX(${-idx * 650}px)`;
 }
+setInterval(carrossel, 1800);
+*/
+const imgs = document.getElementById("img");
+const img = document.querySelector("#img img");
+let idx = 0;
+let screenWidth = window.innerWidth; // Captura a largura da tela
+console.log(screenWidth)
+let deslocamento = (650 * screenWidth)/1920
+
+function carrossel() {
+    idx++;
+    if (idx > 7 - 1) {
+        idx = 0;
+    }
+    
+    // Ajusta o valor de deslocamento de acordo com a largura da tela
+    imgs.style.transform = `translateX(${-idx * deslocamento}px)`;
+    console.log(-idx * deslocamento)
+}
+
 setInterval(carrossel, 1800);
