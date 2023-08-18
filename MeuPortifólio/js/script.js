6a8614f5f3e48350f15b9943e9225b5d79649b16
Lucas Pereira <pereirapro123@gmail.com>
2023-08-18 17:35:28 -0300

    Commit

--- MeuPortifólio/js/script.js
+++ MeuPortifólio/js/script.js
@@ -4,9 +4,10 @@ const img = document.querySelector("#img img")
 let idx = 0;
 function carrossel(){
     idx++;
-    if(idx > 4 -1){
+    if(idx > 7 -1){
         idx=0;
     }
+    
     imgs.style.transform = `translateX(${-idx * 650}px)`;
 }
 setInterval(carrossel, 1800);
