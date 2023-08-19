540253c9943b48f8b1312fa5863308563a872636
PereiraLegend <pereirapro123@gmail.com>
2023-08-18 21:04:24 -0300

    Update script.js

--- MeuPortifólio/js/script.js
+++ MeuPortifólio/js/script.js
@@ -18,7 +18,7 @@ const img = document.querySelector("#img img");
 let idx = 0;
 let screenWidth = window.innerWidth; // Captura a largura da tela
 console.log(screenWidth)
-let deslocamento = (650 * screenWidth)/1920
+//let deslocamento = (650 * screenWidth)/1920
 
 function carrossel() {
     idx++;
@@ -27,8 +27,39 @@ function carrossel() {
     }
     
     // Ajusta o valor de deslocamento de acordo com a largura da tela
-    imgs.style.transform = `translateX(${-idx * deslocamento}px)`;
-    console.log(-idx * deslocamento)
+    imgs.style.transform = `translateX(${-idx * 650}px)`;
+    //console.log(-idx * deslocamento)
 }
 
-setInterval(carrossel, 1800);
+function carrossel2() {
+    idx++;
+    if (idx > 7 - 1) {
+        idx = 0;
+    }
+    
+    // Ajusta o valor de deslocamento de acordo com a largura da tela
+    imgs.style.transform = `translateX(${-idx * 402}px)`;
+    //console.log(-idx * deslocamento)
+}
+
+function carrossel3() {
+    idx++;
+    if (idx > 7 - 1) {
+        idx = 0;
+    }
+    
+    // Ajusta o valor de deslocamento de acordo com a largura da tela
+    imgs.style.transform = `translateX(${-idx * 300}px)`;
+    //console.log(-idx * deslocamento)
+}
+
+if ((screenWidth <= 1980) && (screenWidth >= 1366)){
+    setInterval(carrossel, 1800);
+}
+else if ((screenWidth <= 767) && (screenWidth > 480)) {
+    setInterval(carrossel2, 1800);
+}
+
+else if (screenWidth <= 480) {
+    setInterval(carrossel3, 1800);
+}
